0a1c95507f83db731980d36adfed6e3a731ec1ba
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2024-04-30 10:42:14 +0700

    fix(Installation): Fix typo

--- src/screens/Installation.tsx
+++ src/screens/Installation.tsx
@@ -32,9 +32,14 @@ const Installation: React.FC<InstallationProps> = ({ onConnectMmClick }) => {
     <div>
       <div className={`flex flex-col justify-center items-center`}>
         <Layout className="h-max">
-          <div className="flex items-center justify-between mt-4">
-            <div className="h2-bold text-white-primary">Connect MetaMask Extension</div>
-            <a href="http://silencelaboratories.com/snap" target="_blank" rel="noreferrer">
+          <div className="flex items-center mt-4">
+            <div className="w-[24px]"></div>
+            <div className="h2-bold text-white-primary mx-auto">Connect MetaMask Extension</div>
+            <a
+              className=""
+              href="http://silencelaboratories.com/snap"
+              target="_blank"
+              rel="noreferrer">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -54,8 +59,8 @@ const Installation: React.FC<InstallationProps> = ({ onConnectMmClick }) => {
               </svg>
             </a>
           </div>
-          <p className="b2-regular text-[#B6BAC3] mt-3">
-            Link your MetaMask wallet with Silent Shard Snap to begin.
+          <p className="b2-bold text-[#B6BAC3] mt-3 text-center">
+            Get seamless 2FA security for your MetaMask wallet
           </p>
           <div className="b2-regular p-2 flex items-start rounded-[8px] border bg-[rgba(96,154,250,0.1)] border-[#1e55af] text-[#BFD7FE] my-6">
             <svg
